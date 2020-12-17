709d94d00ccab9de1d1256c57aa181be59d8be28
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-12-17 18:26:37 +0100

    Formateado

--- src/com/cod/Auxiliar.java
+++ src/com/cod/Auxiliar.java
@@ -1,11 +1,18 @@
 package com.cod;
+
 public class Auxiliar {
-String e = "";
-String h = "127.0.0.2";
-Auxiliar(){
-e="anonymous@danielcastelao.org"; }
+    String e = "";
+    String h = "127.0.0.2";
 
-Auxiliar(String param1){ this.e = param1; }
+    Auxiliar() {
+        e = "anonymous@danielcastelao.org";
+    }
 
-public boolean con() {
-if (e!="anonymous@danielcastelao.org") { return true; } else {return false;}}}
+    Auxiliar(String param1) {
+        this.e = param1;
+    }
+
+    public boolean con() {
+        return e != "anonymous@danielcastelao.org";
+    }
+}
